bc81443432f976801705d14950bf33c2ffc946b1
Mahdi732 <ey449751@gmail.com>
2026-02-02 09:59:51 +0100

    add slugify utility function for string normalization and formatting

---
+++ backend/src/common/utils/slugify.ts
@@ -0,0 +1,9 @@
+export const slugify = (value: string): string => {
+  return value
+    .normalize('NFD')
+    .replace(/\p{Diacritic}/gu, '')
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '_')
+    .replace(/^_+|_+$/g, '')
+    .replace(/_+/g, '_');
+};
